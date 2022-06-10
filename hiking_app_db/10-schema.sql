5ad42f43d1d0120bb8699d51a40bffea96899767
Patrick Alarcon <dev@ichaiv.al>
2022-06-10 11:28:58 -0400

    DB Scheme - Alpha

---
+++ hiking_app_db/10-schema.sql
@@ -0,0 +1,61 @@
+drop database if exists hiking_app;
+create database hiking_app;
+use hiking_app;
+
+-- Models
+
+create table trail (
+	trail_id int primary key auto_increment,
+	`name` varchar(100) not null,
+	city varchar(30) not null,
+	state varchar(2) not null,
+	trail_length int not null,
+	rating varchar(20) not null,
+	trail_map varchar(1024) not null,
+	description varchar(4096) null
+);
+
+create table photos(
+	photo_id int primary key auto_increment,
+	photo_url varchar(500)
+);
+
+create table spot (
+	spot_id int primary key auto_increment,
+	`name` varchar(50) null,
+	photo_id int null,
+	gps_lat double not null,
+	gps_long double not null,
+	rating int not null,
+	spot_map varchar(1024) not null,
+	description varchar(4096) null,
+	constraint fk_photos_photo_id
+		foreign key(photo_id)
+		references photos(photo_id)
+);
+
+-- Many to Many table(s)
+create table trail_spot(
+	trail_id int not null,
+	spot_id int not null,
+	constraint pk_trail_spot
+		primary key (trail_id, spot_id),
+	constraint fk_trail_trail_id
+		foreign key(trail_id)
+		references trail(trail_id),
+	constraint fk_spot_spot_id
+		foreign key(spot_id)
+		references spot(spot_id)
+);
+
+-- User entity
+
+create table app_user (
+	app_user_id int primary key auto_increment,
+	email varchar(50) not null unique,
+	password_hash varchar(2048) not null,
+	first_name varchar(20) null,
+	last_name varchar(20) null,
+	city varchar(30) null,
+	state varchar(2) null
+);
