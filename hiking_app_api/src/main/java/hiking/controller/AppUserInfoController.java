6c0ae3f2f413aa9661161a9652793825f6d7657e
Patrick Alarcon <dev@ichaiv.al>
2022-06-13 19:30:57 -0400

    Adjustments to AppUserInfoController

--- hiking_app_api/src/main/java/hiking/controller/AppUserInfoController.java
+++ hiking_app_api/src/main/java/hiking/controller/AppUserInfoController.java
@@ -3,9 +3,14 @@ package hiking.controller;
 import hiking.models.AppUserInfo;
 import hiking.service.AppUserInfoService;
 import hiking.service.Result;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -29,8 +34,18 @@ public class AppUserInfoController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<Object> add(@RequestBody AppUserInfo info){
+    public ResponseEntity<Object> add(@Valid @RequestBody AppUserInfo info, BindingResult bindingResult) {
+
+
+        if(bindingResult.hasErrors()){
+            List<String> messages = bindingResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toList();
+            return new ResponseEntity<>(messages, HttpStatus.BAD_REQUEST);
+        }
+
         Result<AppUserInfo> result = service.add(info);
+
 
         if(result.isSuccess()){
             return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
@@ -45,7 +60,15 @@ public class AppUserInfoController {
     // But considering that the two objects are obstensively linked, I don't think there is a need
     // To specify the id as a path variable, and will instead already be ingrained in the body.
     @PutMapping("/")
-    public ResponseEntity<Object> update(@RequestBody AppUserInfo info){
+    public ResponseEntity<Object> update(@Valid @RequestBody AppUserInfo info, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()){
+            List<String> messages = bindingResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toList();
+            return new ResponseEntity<>(messages, HttpStatus.BAD_REQUEST);
+        }
+
         Result<AppUserInfo> result = service.update(info);
 
         if(result.isSuccess()){
