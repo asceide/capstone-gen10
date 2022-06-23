51a30b585afebf0d5472e5bb2490fb69ccf7742b
Patrick Alarcon <60891908+asceide@users.noreply.github.com>
2022-06-23 10:06:33 -0400

    Env vars updated, dockerfile complete

--- hiking_app_ui/85-set-environment-vars.sh
+++ hiking_app_ui/85-set-environment-vars.sh
@@ -1,5 +1,5 @@
 cat << EOF > /usr/share/nginx/html/config.js
 window.API_URL = "$API_URL";
 window.BUCKET_URL = "$BUCKET_URL";
-windows.MAPS_API_KEY = "$MAPS_API_KEY";
+window.MAPS_API_KEY = "$MAPS_API_KEY";
 EOF
